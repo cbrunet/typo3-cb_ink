3e4510b053883fd5a1c678c90144140955954e33
Charles Brunet <charles@cbrunet.net>
2017-02-05 14:14:05 -0500

    namespace bugfix. updated versions.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,15 +21,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'direct_mail' => '4.0.0-4.0.99',
+			'direct_mail' => '4.0.0-5.1.99',
 		),
 	),
 );
