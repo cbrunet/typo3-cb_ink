b55410c6b62e0cde33e4f1bc25d63643cb6fda0b
Christoph Lehmann <lehmann@networkteam.com>
2017-09-20 18:19:59 +0200

    TASK: Require TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,15 +21,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '8.7.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'direct_mail' => '4.0.0-5.1.99',
+			'direct_mail' => '',
 		),
 	),
 );
